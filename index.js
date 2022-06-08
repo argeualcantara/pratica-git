ed03672992a19f713530ab0eba410b764885cc8f
Argeu Aprigio Alcantara <argeualcantara@users.noreply.github.com>
2022-06-08 20:35:01 -0300

    Add null check bug-fix (#2)

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ app.use(express.json());
 
 app.get('/namespace', (req, res) => {
     nsClient.listNamespace(k8s, req, res);
+
+    if (k8s != null && res != null) {
+      console.log("k8s e res não é null");
+    }
     if (req != null) {
         console.log(req);
         console.log("req não é null");
+
     }
 })
 
