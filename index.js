a6ee0fe4fa4df94b8b337cde868a9bc0db13b3f9
Argeu Alcantara <a.argeu@gmail.com>
2022-06-08 20:23:01 -0300

    Add index js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const k8s = require('@kubernetes/client-node');
+
+const nsClient = require("./clients/namespace");
+const podClient = require("./clients/pod");
+const deployClient = require("./clients/deployment");
+
+const app = express();
+const port = 3000;
+const intervalTimeInMillis = 9000;
+
+app.use(express.json());
+
+app.get('/namespace', (req, res) => {
+    nsClient.listNamespace(k8s, req, res);
+})
+
+app.post('/namespace/:namespace', (req, res) => {
+    nsClient.createNameSpace(k8s, req, res);
+})
+
+app.get('/namespace/:namespace/pods', (req, res) => {
+    podClient.listPodsFromNamespace(k8s, req, res);
+})
+
+app.post('/namespace/:namespace/deploy', (req, res) => {
+  deployClient.deployApp(k8s, req, res);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
